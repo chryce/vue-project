c2b407db9b503125c5bb6530f6f0e79c9e1a5f1f
胡睿毅 <807776962@qq.com>
2022-10-28 23:51:02 +0800

    script setup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,20 @@
+<script setup>
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <HelloWorld msg="哈哈哈"></HelloWorld>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 10vh;
     display: flex;
     align-items: center;
   }
